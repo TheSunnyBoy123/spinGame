a09d8773257989b0ff1497c0da29bd1e88339890
TheSunnyBoy123 <sunrajpatelcph@gmail.com>
2020-10-05 11:20:44 +0200

    initial commit

---
+++ spinGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  spinGame
+//
+//  Created by Rajesh Patel on 05/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
